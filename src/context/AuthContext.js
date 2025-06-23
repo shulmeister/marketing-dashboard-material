355711a7738d9457288b40d9bf1ea2a507fa6f17
shulmeister <jason@coloradocareassist.com>
2025-06-22 22:21:36 -0600

    Fix Prettier formatting errors in AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -96,11 +96,16 @@ export const AuthProvider = ({ children }) => {
   const handleGoogleResponse = async (response) => {
     try {
       // Decode the JWT token
-      const base64Url = response.credential.split('.')[1];
-      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-      const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-      }).join(''));
+      const base64Url = response.credential.split(".")[1];
+      const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+      const jsonPayload = decodeURIComponent(
+        atob(base64)
+          .split("")
+          .map(function (c) {
+            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+          })
+          .join("")
+      );
 
       const userInfo = JSON.parse(jsonPayload);
 
